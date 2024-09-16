2497c5a59b3f6fa3b06d6a5fff331a136638b865
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-09-16 20:58:45 +0330

    v1 debug and test user_account API

--- user_account/tests.py
+++ user_account/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_register(self):
+        url = reverse('register')
+        data = {'username': 'new_user', 'password': 'new_pass'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_login(self):
+        url = reverse('login')
+        data = {'username': 'test', 'password': 'test'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        url = reverse('logout')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_profiles(self):
+        url = reverse('profiles')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)
+
+    def test_get_profile(self):
+        url = reverse('profile', kwargs={'pk': self.user.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.user.username)
+
+    def test_update_profile(self):
+        url = reverse('profile', kwargs={'pk': self.user.id})
+        data = {'username': 'updated_user'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'updated_user')
+
+    def test_unauthorized_profile_update(self):
+        other_user = User.objects.create_user(username='other_user', password='other_pass')
+        url = reverse('profile', args=[other_user.id])
+        data = {'username': 'hacker'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
